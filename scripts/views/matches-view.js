e5c980c6a33fd65565f1c24add0deddb6a0b5090
Jimmy Chang <jimmyfchang94@gmail.com>
2018-04-04 17:06:07 -0700

    connected to petfinder API and got matches!

--- scripts/views/matches-view.js
+++ scripts/views/matches-view.js
@@ -6,9 +6,24 @@ var app = app || {};
 
   let matchesView = {};
 
+  matchesView.render = matches => {
+    let template = Handlebars.compile($('#matches-view-template').text());
+    module.Matches.allShelters.forEach(shelter => {
+      if (matches.shelterId === shelter.shelterId) {
+        matches.shelterName = shelter.name;
+      }
+    });
+    if (matches.shelterName === '' || matches.shelterName === undefined) {
+      matches.shelterName = 'Sorry, we could not find the name of this shelter.';
+    };
+    return template(matches);
+  };
+
+
   matchesView.init = () => {
     $('.container').hide();
     $('#matches-view').fadeIn('slow');
+    module.Matches.all.forEach(x => $('#matches-view').append(matchesView.render(x)));
   };
 
   module.matchesView = matchesView;
